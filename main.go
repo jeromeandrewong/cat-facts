4a81f8ae1ea37db2c84a2e1929882d99dca64eed
Jerome Wong <jeromewongjh@gmail.com>
2023-06-28 02:23:00 +0800

    feat: server and endpoint

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"log"
 	"net/http"
 	"time"
 
@@ -10,6 +11,36 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type Server struct {
+	client *mongo.Client
+}
+
+func NewServer(c *mongo.Client) *Server {
+	return &Server{
+		client: c,
+	}
+}
+
+func (s *Server) handleGetAllFacts(w http.ResponseWriter, r *http.Request) {
+	// get reference to "facts" collection in "catfact" database
+	collection := s.client.Database("catfact").Collection("facts")
+
+	// query that matches all docs in collection
+	query := bson.M{}
+	cursor, err := collection.Find(context.Background(), query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	results := []bson.M{} // slice of bson.M structs to store results
+	if err := cursor.All(context.Background(), &results); err != nil {
+		log.Fatal(err)
+	}
+	w.WriteHeader(http.StatusOK)                       // set status code to 200
+	w.Header().Add("Content-Type", "application/json") // set content type to json
+	json.NewEncoder(w).Encode(results)                 // encode results into json and write to response writer
+
+}
 
 type CatFactWorker struct {
 	client *mongo.Client
@@ -54,5 +85,10 @@ func main() {
 		panic(err)
 	}
 	worker := NewCatFactWorker(client)
-	worker.start()
+	go worker.start()
+
+	server := NewServer(client)
+	http.HandleFunc("/facts", server.handleGetAllFacts)
+
+	http.ListenAndServe(":3000", nil)
 }
